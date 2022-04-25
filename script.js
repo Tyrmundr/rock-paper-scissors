1c1b7035cbfec4006dff020798e1797baa14bbb0
Tyrmundr <taki.96061@gmail.com>
2022-04-25 13:47:42 +0200

    Add function: playRound, add conditional logic to determine winner

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const rps  = ["rock", "paper", "scissors"];
 function computerPlay() {
     return rps[Math.floor(Math.random() * rps.length)];
 }
+
+//Play Round function
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let result = "";
+
+    if(playerSelection === computerSelection) {
+        return result = `It's a DRAW! Both choices are: ${playerSelection}`;
+    } else if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper"))  {
+        return result = `Player WINS, Computer LOSES: ${playerSelection} beats ${computerSelection}`
+    } else {
+        return result = `Computer WINS, Player LOSES: ${computerSelection} beats ${playerSelection}`
+    }
+}
