7aae6dd9dd6b45cd0094eb2e43e0b2a6c2544298
Tyrmundr <taki.96061@gmail.com>
2022-04-25 13:35:09 +0200

    Add global array and random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict"
+
+//Global Array for choices
+const rps  = ["rock", "paper", "scissors"];
+
+//Computer play function
+function computerPlay() {
+    return rps[Math.floor(Math.random() * rps.length)];
+}
