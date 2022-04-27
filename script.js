c5e89a44d41cbaa7cd366a96eb15257d2c15b735
Tyrmundr <taki.96061@gmail.com>
2022-04-27 18:37:21 +0200

    Add utility functions

--- script.js
+++ script.js
@@ -10,11 +10,74 @@ const buttons = document.querySelectorAll("[data-value]")
 buttons.forEach(btn => {
     btn.addEventListener("click", (e) => {
         let playerChoice = e.target.classList[0];
-        playRound(playerChoice)
+        let computerChoice = computerPlay();
+        changePreviousPicks(playerChoice, computerChoice);
+        playRound(playerChoice, computerChoice);
     })
 })
 
 const message = document.querySelector(".msg");
+const previousPlayer = document.querySelector(".previous-player");
+const previousComputer = document.querySelector(".previous-computer");
+const div = document.createElement("div");
+const icon = document.createElement("img");
+
+//Previous picks function
+function changePreviousPicks(playerPick, computerPick) {
+    let playerPicks = [];
+    let computerPicks = [];
+
+    if(checkPlayerPick()) {
+        picksPlayer.pop();
+    } else if(/*check if amount is 2*/X)
+    {
+        div.classList.add("opaquer");
+    } else if(/*check if amount is 1*/X) {
+        div.classList.add("opaque");
+    }
+
+    if(checkComputerPick()) {
+        picksComputer.pop();
+    } else {
+        picksComputer.unshift(computerPick)
+    }
+}
+
+//check for class
+function checkOpacity(array) {
+    const classes = ["opaque", "opaquer"];
+    if(array.length) {
+        if(classes.some(classItem => array[0].classList.contains(classItem))) {
+            array[0].className = ""; 
+        } else if(array[1].className !== "opaque") {
+            array[1].className = "";
+            array[1].classList.add("opaque");
+        } else if(array[2].className !== "opaquer") {
+            array[2].className = "";
+            array[2].classList.add("opaquer");
+        }
+    }
+    
+}
+
+//creating previous pick elements
+function createPick(pick) {
+    div.classList.add("prev");
+
+}
+
+//checking picks
+function checkPlayerPick(prevPlayer = previousPlayer) {
+    if(prevPlayer.childElementCount === 3) {
+        return true;
+    } return;
+}
+
+function checkComputerPick(prevComputer = previousComputer) {
+    if(prevComputer.childElementCount === 3) {
+        return true;
+    } return;
+}
 
 //Computer play function
 function computerPlay() {
@@ -22,7 +85,7 @@ function computerPlay() {
 }
 
 //Play Round function
-function playRound(playerSelection, computerSelection = computerPlay()) {
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerSelection.toLowerCase();
     let result = "";
@@ -35,6 +98,7 @@ function playRound(playerSelection, computerSelection = computerPlay()) {
         result = `Computer won!`
     }
     message.innerText = result;
+    return playerSelection;
 }
 
 //5 round Game function
